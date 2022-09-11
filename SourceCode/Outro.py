c2955ebf1c7c6b987f7c0f9ec0a04251126fec19
Elrish John <98701619+xrishz@users.noreply.github.com>
2022-09-12 07:43:53 +0800

    Add files via upload

--- SourceCode/Outro.py
+++ SourceCode/Outro.py
@@ -1,20 +1,28 @@
 import os
 import time
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1' # sorry Pygame community :(
+import pygame
 import keyboard as keyboard
+from pygame import mixer
 
-file = "outroMusic.wav"
-os.system(file)
+if not os.path.exists("outroMusic.wav"): # check for the music file
+    print("THE MUSIC (outroMusic.wav) IS MISSING")
 
-time.sleep(0.2)
-keyboard.press_and_release("alt + tab")
-time.sleep(0.2)
-keyboard.press_and_release("f11")
-time.sleep(0.1)
+else:
+    outrosound = os.path.join(os.getcwd(), 'outroMusic.wav') # filename
+    mixer.init()
+    mixer.music.load(outrosound)
+    mixer.music.play()
 
-for i in range(10, 0, -1):
-    print("Shutting down in..." + str(i))
-    time.sleep(1)
+    time.sleep(0.2)
+    keyboard.press_and_release("f11") # fullscreen the terminal
+    time.sleep(0.1)
 
-time.sleep(1.5)
+    for i in range(10, 0, -1):
+        print("Shutting down in..." + str(i))
+        time.sleep(1)
 
-os.system("shutdown /s /t 0")
+    time.sleep(1.5)
+
+    os.system("shutdown /s /t 0")
